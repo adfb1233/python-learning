ba778d15bd59d5b7e56e071ad4d08b1a6b7f64fe
adfb1233 <pg118and775@gmail.com>
2025-07-07 14:07:12 +0800

    Add tools module and usage example
    Introduces tools.py with two simple tool functions and a main entry point. Adds use_tools.py to demonstrate importing and using the tool_1 function from tools.py.

---
+++ 0707~0711/tools.py
@@ -0,0 +1,12 @@
+def main():
+    print(f"tolols test {tool_1()}")
+    print(f"tolols test {tool_2()}")
+
+def tool_1():
+    return "you use tool1"
+def tool_2():
+    return "you use tool2"
+
+if __name__ == '__main__':
+    main()
+print(f"tools.py __name__ is {__name__}")
---
+++ 0707~0711/use_tools.py
@@ -0,0 +1,5 @@
+
+import tools
+print(f"use_tools  {tools.tool_1()}")
+#print(f"tools.py __name__ is {__name__}")
+
